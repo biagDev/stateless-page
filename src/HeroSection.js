0dbbc04e33c60c3eb139fe782ac65be4bee09459
Biagio Dev <biagio@biag.dev>
2022-09-16 15:21:21 -0400

    hero section completed

---
+++ src/HeroSection.js
@@ -0,0 +1,14 @@
+function HeroSection() {
+  return (
+    <div className='hero-section'>
+      <nav>
+        <p>HOME</p>
+        <p>OUR SERVICES</p>
+        <p>SUBSCRIBE</p>
+      </nav>
+      <h2>we do websites right</h2>
+    </div>
+  );
+}
+
+export default HeroSection;
