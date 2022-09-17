50ad03d36457fb689ca44fc51e3d9cf1feba9dd3
Biagio Dev <biagio@biag.dev>
2022-09-17 15:40:54 -0400

    footer section and mobile view completed

--- src/FooterSection.js
+++ src/FooterSection.js
@@ -1,5 +1,11 @@
 function FooterSection() {
-  return <div>hey there</div>;
+  return (
+    <div className='footer-container'>
+      <h2>subscribe to our email list</h2>
+      <input type='text' placeholder={"What's your email?"} />
+      <button>Submit</button>
+    </div>
+  );
 }
 
 export default FooterSection;
